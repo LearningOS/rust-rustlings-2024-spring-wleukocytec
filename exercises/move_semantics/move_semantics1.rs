eaa90409d9946ac2867bcd7df83b82e26e4e22d5
wleukocytec <wleukocytec@gmail.com>
2024-07-12 00:12:20 +0800

    vecs and move_semantics

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -8,7 +8,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -18,11 +18,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    let vec = vec;
 
     vec
 }
